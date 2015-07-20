7b6cfd163f2ba10473fa4088f42f746cae41cdec
bx5a <bx5a.develop@gmail.com>
2015-07-20 23:48:55 +0200

    Add the option to download before unpacking

--- helper.cmake
+++ helper.cmake
@@ -2,13 +2,34 @@ cmake_minimum_required(VERSION 3.1)
 
 ########################################
 # Unpack a zip or tar.gz
-# args: SOURCE DESTINATION
+# args: SOURCE DESTINATION.
+# DOWNLOAD is optional. Set it if SOURCE is an URL
 # returns: OPENSSL_ROOT_DIR and OPENSSL_LIBRARIES
 
 function(Unpack)
   # parse args
+  set(optional_args DOWNLOAD)
   set(one_val_args SOURCE DESTINATION)
-  cmake_parse_arguments(Unpack  "" "${one_val_args}" "" ${ARGN})
+  cmake_parse_arguments(Unpack  "${optional_args}" "${one_val_args}" "" ${ARGN})
+
+  get_filename_component(source_filename ${Unpack_SOURCE} NAME)
+  # Download if needed
+  if (${Unpack_DOWNLOAD})
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/download)
+    set(downloaded_path ${CMAKE_BINARY_DIR}/download/${source_filename})
+
+    if (NOT EXISTS ${downloaded_path})
+      message(STATUS "Unpack - Downloading...")
+      file(DOWNLOAD 
+        ${Unpack_SOURCE}
+        ${downloaded_path}
+      )
+    endif()
+
+    set(Unpack_SOURCE ${downloaded_path})
+    message(STATUS "Unpack - Found downloaded archive ${source_filename}")
+  endif()
+
   # check if doesn't exists
   if (NOT EXISTS ${Unpack_DESTINATION})
     get_filename_component(source_filename ${Unpack_SOURCE} NAME)
