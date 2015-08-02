328f70ac3216c4e0159b3d7ed4240f1f7df9b054
bx5a <bx5a.develop@gmail.com>
2015-08-02 16:11:44 +0200

    Create an AddTarget function to order files in project

--- helper.cmake
+++ helper.cmake
@@ -49,3 +49,59 @@ function(Unpack)
     endif()
   endif()
 endfunction(Unpack)
+
+cmake_minimum_required(VERSION 3.1)
+
+
+###################################
+# Create a target
+# args: TYPE [SHARED/STATIC] SOURCES LINK
+
+function(AddTarget)
+  # parse args
+  set(option_args SHARED STATIC)
+  set(one_val_args TYPE)
+  set(multiple_val_args SOURCES LINK)
+  cmake_parse_arguments(AddTarget "${option_args}" "${one_val_args}" "${multiple_val_args}" ${ARGN})
+
+  # switch target type
+  string(TOLOWER "${AddTarget_TYPE}" lower_case_TYPE)
+  if (${lower_case_TYPE} STREQUAL "executable")
+    add_executable(${ARGV0} "${AddTarget_SOURCES}")
+  else()
+    # for libraries, switch SHARED or STATIC. Default is SHARED
+    if(${add_target_STATIC})
+      add_library(${ARGV0} STATIC "${AddTarget_SOURCES}")
+    else()
+      add_library(${ARGV0} SHARED "${AddTarget_SOURCES}")
+    endif()
+  endif()
+
+  # organize sources in group
+  foreach(source_file ${AddTarget_SOURCES})
+    # find if source or header
+    get_filename_component(source_ext ${source_file} EXT)
+    set(group_root_name "source")
+    if(${source_ext} STREQUAL ".h")
+      set(group_root_name "header")
+    endif()
+    # create a group name from the file path
+    get_filename_component(absolute_path ${source_file} ABSOLUTE)
+    string(REPLACE ${CMAKE_CURRENT_SOURCE_DIR} ${group_root_name} relative_path ${absolute_path})
+    get_filename_component(relative_folder ${relative_path} DIRECTORY)
+    string(REPLACE "/" "\\" group_name ${relative_folder})
+    # add the file to the right group
+    source_group(${group_name} FILES ${source_file})
+  endforeach()
+
+  # link to desired libraries
+  # sometimes the library can be prefixed by debug/optimized/general
+  foreach(source_file ${AddTarget_LINK})
+    if (${source_file} STREQUAL "debug" OR ${source_file} STREQUAL "optimized" OR ${source_file} STREQUAL "general")
+      set(link_prefix ${source_file})
+    else ()
+      target_link_libraries(${ARGV0} ${link_prefix} "${source_file}")
+      unset(link_prefix)
+    endif()
+  endforeach()
+endfunction(AddTarget)
