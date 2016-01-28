b792a27370485b39b0727bdff98392ccb37a662f
bx5a <bx5a.develop@gmail.com>
2016-01-28 16:43:18 +0100

    Enable static or shared runtime for find boost. Fix debug/release library for msvc

--- boost.cmake
+++ boost.cmake
@@ -7,26 +7,35 @@ cmake_minimum_required(VERSION 3.1)
 
 function(FindBoost)
   # parse args
-  set(option_args STATIC SHARED)
+  set(option_args STATIC SHARED RUNTIME_STATIC RUNTIME_SHARED)
   set(multiple_val_args COMPONENTS)
   set(one_val_args SOURCE_DIR)
-  cmake_parse_arguments(FindBoost 
-                        "${option_args}" "${one_val_args}" "${multiple_val_args}" ${ARGN})
+  cmake_parse_arguments(FindBoost
+                        "${option_args}"
+                        "${one_val_args}"
+                        "${multiple_val_args}"
+                        ${ARGN})
 
   # default is static link type
   set(boost_link_type static)
   set(Boost_USE_STATIC_LIBS ON)
-  
+
   if (${FindBoost_SHARED})
     add_definitions(-DBOOST_LOG_DYN_LINK)
     set(boost_link_type shared)
     set(Boost_USE_STATIC_LIBS OFF)
   endif()
 
-  # TODO(bx5a): handle runtime_link in parameters
-  set(boost_runtime_link_type shared)
+  # default runtime type is static
+  # TODO(bx5a): if lib were already built with the wrong runtime, rebuild it
+  # instead of using the existing ones
+  set(boost_runtime_link_type static)
+  if (${FindBoost_RUNTIME_SHARED})
+    set(boost_runtime_link_type shared)
+  endif()
 
-  # variables 
+
+  # variables
   set(boost_build_dir ${CMAKE_BINARY_DIR}/thirdparty/boost)
   set(boost_sources ${FindBoost_SOURCE_DIR})
   set(boost_b2_command ./b2)
@@ -43,7 +52,7 @@ function(FindBoost)
 
   # try to find package first.
   find_package(Boost
-    COMPONENTS 
+    COMPONENTS
       ${FindBoost_COMPONENTS})
 
   # If fail, build using b2 and try to find again
@@ -55,13 +64,13 @@ function(FindBoost)
 
     # configure
     message(STATUS "Boost - Bootstrap configure ${boost_sources}")
-    execute_process(COMMAND ${boost_bootstrap_command} 
+    execute_process(COMMAND ${boost_bootstrap_command}
                     WORKING_DIRECTORY ${boost_sources}
-                    RESULT_VARIABLE cmd_result 
-                    OUTPUT_VARIABLE cmd_output 
+                    RESULT_VARIABLE cmd_result
+                    OUTPUT_VARIABLE cmd_output
                     ERROR_VARIABLE cmd_error)
     if(NOT cmd_result EQUAL "0")
-      message(FATAL_ERROR 
+      message(FATAL_ERROR
         "Failed running ${boost_bootstrap_command}:\n${cmd_output}\n${cmd_error}\n")
     endif()
 
@@ -70,22 +79,23 @@ function(FindBoost)
 
     # build
     message(STATUS "Boost - Building")
-    
+
     set(boost_b2_options
       ${b2_components}
     	--build-dir=${boost_build_dir}
-      --prefix=${boost_build_dir} 
+      --prefix=${boost_build_dir}
       --stagedir=${boost_build_dir}
       link=${boost_link_type}
-      runtime-link=${boost_runtime_link_type})
+      runtime-link=${boost_runtime_link_type}
+      variant=debug,release)
 
-    execute_process(COMMAND ${boost_b2_command} ${boost_b2_options} 
+    execute_process(COMMAND ${boost_b2_command} ${boost_b2_options}
                     WORKING_DIRECTORY ${boost_sources}
-    				        RESULT_VARIABLE cmd_result 
-                    OUTPUT_VARIABLE cmd_output 
+    				        RESULT_VARIABLE cmd_result
+                    OUTPUT_VARIABLE cmd_output
                     ERROR_VARIABLE cmd_error)
     if(NOT cmd_result EQUAL "0")
-      message(FATAL_ERROR 
+      message(FATAL_ERROR
         "Failed running ${boost_b2_command}:\n${cmd_output}\n${cmd_error}\n")
     endif()
 
@@ -94,10 +104,23 @@ function(FindBoost)
     # try to find again
     find_package(Boost
       REQUIRED
-      COMPONENTS 
+      COMPONENTS
         ${FindBoost_COMPONENTS})
   endif()
-  
+
+  # fix for visual studio: split release and debug libraries
+  if(MSVC)
+    foreach(library ${Boost_LIBRARIES})
+      # add debug flag to separate release/debug libraries
+      string(REGEX REPLACE "-s-" "-sgd-" debug_library "${library}")
+      set(Boost_debug_and_release_LIBRARIES
+      "${Boost_debug_and_release_LIBRARIES}"
+      "optimized" "${library}"
+      "debug" "${debug_library}")
+    endforeach()
+    set(Boost_LIBRARIES ${Boost_debug_and_release_LIBRARIES})
+  endif()
+
   # set variable scope
   set(Boost_LIBRARIES ${Boost_LIBRARIES} PARENT_SCOPE)
   set(BOOST_ROOT ${boost_build_dir} PARENT_SCOPE)
