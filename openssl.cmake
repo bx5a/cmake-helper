9cd5470de74d62f5626a2efadc8a40f97fe613e6
bx5a <bx5a.develop@gmail.com>
2016-02-22 12:30:53 +0100

    Add static/shared option to openssl and force use of built version

--- openssl.cmake
+++ openssl.cmake
@@ -8,30 +8,65 @@ cmake_minimum_required(VERSION 3.1)
 function(FindOpenSSL)
   # parse args
   set(one_val_args SOURCE_DIR MINIMUM_VERSION)
+  set(options STATIC SHARED)
   cmake_parse_arguments(FindOpenSSL 
-    "" "${one_val_args}" "" ${ARGN})
+    "${options}" "${one_val_args}" "" ${ARGN})
 
   set(openssl_sources ${FindOpenSSL_SOURCE_DIR})
   set(openssl_build_dir ${CMAKE_BINARY_DIR}/thirdparty/openssl)
+  set(use_static ${FindOpenSSL_STATIC})
+  set(use_shared ${FindOpenSSL_SHARED})
 
 
-  set(OPENSSL_ROOT_DIR "${openssl_build_dir}")
-  set(OPENSSL_LIBRARIES "${openssl_build_dir}/lib")
+  if (${use_static}) 
+    if(WIN32)
+      set(lib_suffix .lib)
+    else()
+      set(lib_suffix .a)
+    endif()
+  elseif(${use_shared})
+    if(WIN32)
+      set(lib_suffix .dll)
+    elseif(APPLE)
+      set(lib_suffix .dylib)
+    else()
+      set(lib_suffix .so)
+    endif()
+  endif() 
+  set(lib_suffix ${lib_suffix})
 
   # create thirdparty folder
   file(MAKE_DIRECTORY ${openssl_build_dir})
 
-  message(${OPENSSL_ROOT_DIR})
-
   # first, try to find
   find_package(OpenSSL ${FindOpenSSL_MINIMUM_VERSION})
+  # check if found libraries has the right extension
+  foreach(library ${OPENSSL_LIBRARIES})
+    get_filename_component(extension ${library} EXT)
+    if (NOT "${CMAKE_FIND_LIBRARY_SUFFIXES}" STREQUAL "${extension}")
+      set(OPENSSL_FOUND FALSE)
+    endif()
+  endforeach()
+
+  if (NOT OPENSSL_FOUND)
+    # force use of previously built libraries
+    set(OPENSSL_LIBRARIES 
+          "${openssl_build_dir}/lib/libssl${lib_suffix}" 
+          "${openssl_build_dir}/lib/libcrypto${lib_suffix}")
+    set(OPENSSL_FOUND TRUE)
+    foreach(library ${OPENSSL_LIBRARIES})
+      if(NOT EXISTS ${library})
+        set(OPENSSL_FOUND FALSE)
+      endif()
+    endforeach()
+  endif()
 
   if (NOT OPENSSL_FOUND)
     # do build    
     message(STATUS "OpenSSL - Configure")
 
     execute_process(COMMAND 
-      ./Configure darwin64-x86_64-cc --prefix=${openssl_build_dir} 
+      ./Configure darwin64-x86_64-cc --prefix=${openssl_build_dir} --shared
       WORKING_DIRECTORY ${openssl_sources}
       RESULT_VARIABLE cmd_result 
       OUTPUT_VARIABLE cmd_output 
@@ -75,7 +110,7 @@ function(FindOpenSSL)
     # try to build once more
     message(STATUS "OpenSSL - Installed")
     message(WARNING "OpenSSL - You might have to remove CMakeCache.txt and CMakeFiles/ for your new build to be taken into account")
-    find_package(OpenSSL ${FindOpenSSL_MINIMUM_VERSION} REQUIRED)
+    find_package(OpenSSL ${FindOpenSSL_MINIMUM_VERSION})
   endif()
 
   set(OPENSSL_ROOT_DIR "${openssl_sources}" PARENT_SCOPE)
